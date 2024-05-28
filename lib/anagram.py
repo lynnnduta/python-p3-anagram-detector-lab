66cb876abcf283e9df6b423fca0fbd5ae5b66a1c
lynn <lynnnduta23@gmail.com>
2024-05-28 19:27:28 +0300

    Add

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+
+        self.word = word
+        self.sorted_word = "".join(sorted(word.lower()))
+
+    def match(self, word_list):
+
+        matches = []
+
+        for candidate in word_list:
+
+            if "".join(sorted(candidate.lower())) == self.sorted_word:
+                matches.append(candidate)
+
+        return matches
